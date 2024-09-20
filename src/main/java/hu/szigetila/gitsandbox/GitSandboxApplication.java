0a1e612cafb0f5c81a7e01cf9a5c13daabd2492e
szigetila <lajos.szigeti@idomsoft.hu>
2024-09-20 10:10:30 +0200

    spring boot

---
+++ src/main/java/hu/szigetila/gitsandbox/GitSandboxApplication.java
@@ -0,0 +1,13 @@
+package hu.szigetila.gitsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitSandboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/szigetila/gitsandbox/GitSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.szigetila.gitsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
