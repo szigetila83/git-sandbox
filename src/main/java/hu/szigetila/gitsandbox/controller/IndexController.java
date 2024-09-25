ca04b43b33cfc03ad945bb29036792031b56d72f
szigetila <lajos.szigeti@idomsoft.hu>
2024-09-25 08:54:44 +0200

    IndexController v2

--- src/main/java/hu/szigetila/gitsandbox/controller/IndexController.java
+++ src/main/java/hu/szigetila/gitsandbox/controller/IndexController.java
@@ -10,7 +10,7 @@ public class IndexController {
 
     @GetMapping("/")
     public String index() {
-        log.info("/index v1");
+        log.info("/index v2");
         return "index";
     }
 }
